23e66c391d05c8c6e6891ce1ab60e36e170c9403
Mezzya <andrey@cryptex.com.ua>
2016-08-14 02:04:03 +0300

    Треугольник

--- JavaOOP/src/Homework02/Main.java
+++ JavaOOP/src/Homework02/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Создадим треугольник
+		Triangle triOne = new Triangle(53, 5, 2);
+		System.out.println(triOne.square());
+		Triangle triTwo = new Triangle(5, 5, 5);
+		System.out.println(triTwo.square());
+
 	}
 
 }
---
+++ JavaOOP/src/Homework02/Triangle.java
@@ -0,0 +1,60 @@
+package Homework02;
+
+public class Triangle {
+	private double a;
+	private double b;
+	private double c;
+	private boolean exist; // Признак существования треугольника
+	
+	public Triangle() {
+		super();
+	}
+	
+	public Triangle(double a, double b, double c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		exist();
+	}
+
+	public double getA() {
+		return a;
+	}
+	public double getB() {
+		return b;
+	}
+	public double getC() {
+		return c;
+	}
+	
+	private void exist()
+	{
+//		Проверим существование треугольника
+		
+		if ((a<(b+c))&&(b<(a+c))&&(c<(a+b)))
+		{
+			exist=true;
+			System.out.println("Треугольник со сторонами A="+a+" B="+b+" C="+c+ " существует");
+		}
+		else System.out.println("Треугольник со сторонами A="+a+" B="+b+" C="+c+ " не существует");
+	}
+	
+	public double square() {
+		
+		if (exist)
+		{
+//			Вычисляем площадь по формуле Герона
+			double p=(a+b+c)/2;
+			double s=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+			return s;
+		}
+		else
+		{
+			System.out.println("Треугольник с такими сторонами не существует. Ну и площадь его тоже.");
+			return 0;
+		}
+		
+	}
+
+}
