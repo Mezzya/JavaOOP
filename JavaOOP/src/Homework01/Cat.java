d4c967851b72cc19f2370c4cfb91cccc1764295a
Mezzya <andrey@cryptex.com.ua>
2016-08-14 01:22:51 +0300

    Про котов вроде бы все

--- JavaOOP/src/Homework01/Cat.java
+++ JavaOOP/src/Homework01/Cat.java
@@ -1,5 +1,88 @@
 package Homework01;
 
 public class Cat {
+	private String name;
+	private int age;
+	private double weight;
+	private boolean alive; // Признак жизнедеятельности
+
+	public Cat() {
+		super();
+	}
+
+	public Cat(String name, int age, double weight) {
+		super();
+		this.name = name;
+		this.age = age;
+		// Кошка рождается живой
+		alive = true;
+		this.weight = weight;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isAlive() {
+		return alive;
+	}
+
+	public void voice() {
+		if (alive)
+			System.out.println(name + " - Мяу-мяу");
+		else
+			System.out.println(name + " - Неполучается мяукнуть. Сдохла я");
+
+	}
+
+	public void eat(double weight) {
+		if (alive)
+			this.weight += weight;
+		else
+			System.out.println(name + " - Мертвые не едят");
+	}
+
+	public void eat(Cat cat) {
+		if (!isAlive()){
+			System.out.println(name + " - Мертвые не едят");
+			return;
+		}
+		if (cat.equals(this)) {
+			System.out.println(name + " - Нее. Себя есть не буду");
+			return;
+		}
+		if (cat.isAlive()) {
+			this.weight += cat.weight;
+			cat.kill();
+			System.out.println(name + " - съел " + cat.name + ". Есть то хочется... ");
+		} else
+			System.out.println(name + " - Вредно есть мертвых котов");
+
+	}
+
+	public void kill() {
+		if (isAlive()) {
+			alive=false;
+			System.out.println(name+" - Всем пока. Умерла я.");
+		} 
+		else System.out.println(name+" - Я уже мертвая.");
+	}
+
+	@Override
+	public String toString() {
+		return name + ", age=" + age + ", weight=" + weight + ", alive=" + alive + "]";
+	}
 
 }
--- JavaOOP/src/Homework01/Main.java
+++ JavaOOP/src/Homework01/Main.java
@@ -5,6 +5,53 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Cat catOne = new Cat("Мурка",1,1.0);
+		Cat catTwo = new Cat("Васька",2,2.5);
+		Cat catThree = new Cat("Неудачник",4,6.0);
+//		Голос
+		catOne.voice();
+		catTwo.voice();
+		
+//		Убили кошку
+		catOne.kill();
+		catOne.voice();
+		
+//		Выведем посмотрим
+		System.out.println();
+		System.out.println(catOne);
+		System.out.println(catTwo);
+		System.out.println(catThree);
+		
+		
+//		Второя поела чутка
+		catTwo.eat(0.250);
+		
+//		Первая попробовала поесть
+		catOne.eat(2);
+		
+//		Выведем посмотрим
+		System.out.println();
+		System.out.println(catOne);
+		System.out.println(catTwo);
+		System.out.println(catThree);
+		
+//		Попробуем сьесть мертвого кота
+		catTwo.eat(catOne);
+		
+//		Попробуем сьесть себя
+		catTwo.eat(catTwo);
+		
+//		Сьедим живого и поправимся
+		
+		catTwo.eat(catThree);
+		
+
+//		Выведем посмотрим
+		System.out.println();
+		System.out.println(catOne);
+		System.out.println(catTwo);
+		System.out.println(catThree);
+
 	}
 
 }
