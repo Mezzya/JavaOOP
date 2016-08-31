aad4f7336bfcab6c604f5e87f87a421f5d417f2e
Mezz <andrey@cryptex.com.ua>
2016-09-01 01:24:04 +0300

    Копирование файлов IO vs NIO

--- JavaOOP/src/Homework51/Main.java
+++ JavaOOP/src/Homework51/Main.java
@@ -1,58 +1,27 @@
 package Homework51;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.sql.Time;
-import java.util.Arrays;
-import java.util.Date;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		File dirOut = new File("DIROUT");
-		File dirIn = new File("DIRIN");
+		File dirOut = new File("/home/mezz/Documents/OUT");
+		File dirIn = new File("/home/mezz/Documents/IN");
 
-		// Проверим сущетвование дирикторий и то что это не файлы
-		if (!dirOut.exists()) {
-			System.err.println(dirOut + " не существует");
-			return;
-		}
-		if (!dirOut.isDirectory()) {
-			System.err.println(dirOut + " не директория");
-			return;
-		}
-
-		if (!dirIn.exists()) {
-			System.err.println(dirIn + " не существует");
-			return;
-		}
-		if (!dirIn.isDirectory()) {
-			System.err.println(dirIn + " не директория");
-			return;
-		}
-
-
-		FileManager fm = new FileManager();
 		String[] arg = { "txt", "bmp", "jpg" };
+
 		try {
-		
-			fm.copyMaskNio(dirOut, dirIn, arg, false);
-			
-		
-			
-			fm.copyMaskIo(dirOut, dirIn, arg);
-			
-			
-			
+
+			FileManager.copyMaskNio(dirOut, dirIn, arg, false);
+			FileManager.copyMaskIo(dirOut, dirIn, arg);
+
 		} catch (IOException e) {
 			System.out.println(e);
 		}
 
-		
-		
 	}
 
 }
