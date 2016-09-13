783cc87189b6c4d3e40d1a6d9eb672d9e25d920e
Mezzya <andrey@cryptex.com.ua>
2016-09-13 20:56:07 +0300

    Refactor

--- JavaOOP/src/HomeWork93/CharCount.java
+++ JavaOOP/src/HomeWork93/CharCount.java
@@ -35,11 +35,6 @@ public class CharCount {
 		relFrec = (double) size / count;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#hashCode()
-	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -48,11 +43,6 @@ public class CharCount {
 		return result;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
