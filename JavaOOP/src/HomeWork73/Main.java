945a223fd0e302df47a1eb8442a402fcbcb376c0
Mezz <andrey@cryptex.com.ua>
2016-09-14 15:58:32 +0300

    Поиск  фала с ограниченным количеством потоков

--- JavaOOP/src/HomeWork73/Main.java
+++ JavaOOP/src/HomeWork73/Main.java
@@ -1,14 +1,33 @@
 package HomeWork73;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		new Thread(new SearchFile(new File("/home/mezz/Documents/OUT"), "test.txt")).start();
-		
+
+		ExecutorService exec = Executors.newCachedThreadPool();
+
+		String tmp = "";
+
+		try {
+			if ((tmp = exec.submit(new SearchFile(new File("/home/mezz/Documents/OUT"), "test.txt", exec))
+					.get()) != null) {
+				System.out.println(tmp);
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
--- JavaOOP/src/HomeWork73/SearchFile.java
+++ JavaOOP/src/HomeWork73/SearchFile.java
@@ -1,43 +1,63 @@
 package HomeWork73;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
-public class SearchFile implements Runnable {
+public class SearchFile implements Callable<String> {
 
 	private File dir;
 	private String filename;
 	private File[] dirs;
-
-	
+	private ExecutorService exec;
+	private ArrayList<Future<String>> result = new ArrayList<>();
 
 	public SearchFile() {
 		super();
 	}
 
-	public SearchFile(File dir, String filename) {
+	public SearchFile(File dir, String filename, ExecutorService exec) {
 		super();
 		this.dir = dir;
 		this.filename = filename;
+		this.exec = exec;
 	}
 
 	@Override
-	public void run() {
+	public String call() {
 
 		dirs = dir.listFiles();
-
 		for (File file : dirs) {
 			if (file.isFile() && file.getName().equals(filename)) {
 				// Нашли файлик
-				System.out.println("Файл " + filename + " найден в " + dir);
+				return "Файл " + filename + " найден в " + dir;
 			}
 
 			if (file.isDirectory()) {
 				// Ищем в директории
-				new Thread(new SearchFile(file, filename)).start();
-
+				result.add(exec.submit(new SearchFile(file, filename, exec)));
 			}
 
 		}
+
+		for (Future<String> res : result) {
+			String tmp = "";
+			try {
+				tmp = res.get();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (tmp != null)
+				System.out.println(tmp);
+		}
+		return null;
 
 	}
 
